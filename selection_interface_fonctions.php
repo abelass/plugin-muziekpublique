c56931802da3d9b0ddd317b318c35ba5da7fa397
abelass <websolutions@mychacra.net>
2012-11-01 12:49:30 +0100

    afficher la rubrique unilingue également quand toutes les rubriques normales sont séléctionnées

--- selection_interface_fonctions.php
+++ selection_interface_fonctions.php
@@ -112,13 +112,12 @@ if ($_GET["descendre_ordre"] > 0) {
 			"ordre>'$ordre'"		
 			);
 		$result2 = sql_select("*", "spip_pb_selection", $where, "ordre LIMIT 0,1");
-		if ($row2 = sql_fetch($result2)) {echo 2;
+		if ($row2 = sql_fetch($result2)) {
 			$ordre_suiv = $row2["ordre"];
 			$art_suiv = $row2["id_article"];
 
 			sql_updateq("spip_pb_selection", array("ordre" => $ordre_suiv), "id_rubrique = '$id_rubrique'  AND id_article='$descendre'");
 			sql_updateq("spip_pb_selection", array("ordre" => $ordre), "id_rubrique = '$id_rubrique'  AND id_article='$art_suiv'");
-
 		}
 	
 	}
